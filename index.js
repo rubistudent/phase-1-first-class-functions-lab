d570e2b3956a7d69384136d37ce6719f9c6a05b4
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2022-08-29 14:47:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+function receivesAFunction(callback){
+    return callback()
+}
+
+function returnsANamedFunction(){
+   return function hello(){
+   }
+}
+
+function returnsAnAnonymousFunction(){
+
+   return function(){
+
+   }
+}
